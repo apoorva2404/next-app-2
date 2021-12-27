1b1693c5bb0682f9b0d2c083d617dc909e9c9316
Apoorva <apoorva@192.168.1.10>
2021-12-27 20:42:24 +0530

    Add

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+// Path: /.eslintrc.js
+
+module.exports = {
+  extends: ['next/core-web-vitals', 'airbnb', 'plugin:react/jsx-runtime'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'react/jsx-closing-bracket-location': 'off',
+  },
+};
